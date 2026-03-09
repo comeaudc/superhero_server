006815aeb744372c7b65d477e3f460325028d946
DylanC <comeaudc@comcast.net>
2026-03-09 12:06:00 -0400

    Added Cors origin and rate limiting

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { logReq, globalErr } from "./middleware/middlewares.js";
 import connectDB from "./db/conn.js";
 import characterRoutes from "./routes/characterRoutes.js";
+import { rateLimit } from "express-rate-limit";
 import cors from "cors";
 
 // Setups
@@ -12,8 +13,21 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 connectDB();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // how long open period
+  limit: 100,
+  message: "Too many requests, please try again later",
+});
+
 // Middleware
-app.use(cors({ origin: "https://superhero-fe.onrender.com" })); // Allows sharing resources to all Servers
+app.use(
+  cors({
+    origin: ["https://superhero-fe.onrender.com", "http://localhost:5173"],
+    credentials: true, // Allow the req.header AND req.body through. w/o would only allow the req.body
+  }),
+); // Allows sharing resources to all Servers
+
+app.use(limiter);
 app.use(express.json());
 app.use(logReq);
 
